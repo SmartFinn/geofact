55ab91af22d1077ebb0981ecf253e611cb59e648
Serhii Yeremenko <finalitik@gmail.com>
2025-04-06 19:29:22 +0300

    Add searchbox using mablibre-gl-geocoder + nominatim

--- js/app.js
+++ js/app.js
@@ -565,3 +565,57 @@ map.on('touchmove', () => {
 map.on('touchend', () => {
   clearTimeout(touchTimer); // Cancel if released early
 });
+
+// Geocoder
+const geocoderApi = {
+  forwardGeocode: async (config) => {
+    const features = [];
+    try {
+      const request =
+        `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson&polygon_geojson=1&addressdetails=1&featureType=city&countrycodes=ua,ru`;
+      const response = await fetch(request);
+      const geojson = await response.json();
+      for (const feature of geojson.features) {
+        const center = [
+          feature.bbox[0] +
+          (feature.bbox[2] - feature.bbox[0]) / 2,
+          feature.bbox[1] +
+          (feature.bbox[3] - feature.bbox[1]) / 2
+        ];
+        const point = {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: center
+          },
+          place_name: feature.properties.display_name,
+          properties: feature.properties,
+          text: feature.properties.display_name,
+          place_type: ['place'],
+          center
+        };
+        features.push(point);
+      }
+    } catch (e) {
+      console.error(`Failed to forwardGeocode with error: ${e}`);
+    }
+
+    return {
+      features
+    };
+  }
+};
+map.addControl(
+  new MaplibreGeocoder(geocoderApi, {
+    maplibregl: maplibregl,
+    collapsed: true,
+    marker: false,
+    showResultMarkers: false,
+    showResultsWhileTyping: true,
+    minLength: 3,
+    debounceSearch: 400,
+    flyTo: {
+      zoom: 13,
+    },
+  }), 'top-left'
+);
